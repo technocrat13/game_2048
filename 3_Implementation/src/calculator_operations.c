fabeaad18b485295470c4f2ea6b3d29919df4c54
Ayush Bishnoi <ayushbishnoi@live.com>
2021-11-18 14:47:56 +0530

    base logic added

--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -1,24 +1,140 @@
-#include <calculator_operations.h>
+#include "calculator_operations.h"
 
-int add(int operand1, int operand2)
+void setup_gameboard()
 {
-    return operand1 + operand2;
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            gameboard[j][i] = 0;
+        }
+    }
 }
 
-int subtract(int operand1, int operand2)
+void print_gameboard()
 {
-    return operand1 - operand2;
+    printf("\n");
+    for (int j = 0; j < n; j++)
+    {
+        printf("       ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", gameboard[j][i]);
+        }
+        printf("\n");
+    }
 }
 
-int multiply(int operand1, int operand2)
+void pick_random_tile()
 {
-    return operand1 * operand2;
-}
 
-int divide(int operand1, int operand2)
-{
-    if(0 == operand2)
-        return 0;
+    int j = rand() % 4;
+    int i = rand() % 4;
+    if (gameboard[j][i] == 0)
+    {
+        if (rand() % 3 == 0)
+        {
+            gameboard[j][i] = 4;
+        }
+        else
+        {
+            gameboard[j][i] = 2;
+        }
+    }
     else
-        return operand1 / operand2;
+    {
+        pick_random_tile();
+    }
+}
+
+void slam_left()
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int t = 0; t < n - 1; t++)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                if(gameboard[j][i] == 0)
+                {
+                    gameboard[j][i] = gameboard[j][i + 1];
+                    gameboard[j][i + 1] = 0;
+                }
+            }
+        }
+    }
+}
+
+void compress_left()
+{
+    for(int j = 0; j < n; j++)
+    {
+        for(int i = 0; i < n -1; i++)
+        {
+            if(gameboard[j][i] != 0 && gameboard[j][i] == gameboard[j][i + 1])
+            {
+                gameboard[j][i] = gameboard[j][i] * 2;
+                gameboard[j][i + 1] = 0;
+            }
+        }
+    }
+}
+
+void transpose()
+{
+    int interim_gameboard[n][n]; 
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            interim_gameboard[i][j] = gameboard[j][i];
+        }
+    }
+    memcpy(&gameboard[0][0], &interim_gameboard[0][0], n * n * sizeof(interim_gameboard[0][0]));
+    //gameboard =  *interim_gameboard;
+}
+
+void reverse()
+{
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n / 2; i++)
+        {
+            int temp = gameboard[j][i];
+            gameboard[j][i] = gameboard[j][n - i - 1];
+            gameboard[j][n - i - 1] = temp;
+        }
+    }
+}
+
+/* Start of the application */
+int main()
+{
+    srand(time(0));
+    printf("\n****2048****\n");
+
+    setup_gameboard();
+    print_gameboard();
+    pick_random_tile();
+    pick_random_tile();
+    print_gameboard();
+    slam_left();
+    print_gameboard();
+    compress_left();
+    print_gameboard();
+    slam_left();
+    print_gameboard();
+    pick_random_tile();
+    pick_random_tile();
+    pick_random_tile();
+    pick_random_tile();
+
+    print_gameboard();
+    printf("transpose: ");
+    transpose();
+
+    print_gameboard();
+    printf("reverse: ");
+    reverse();
+    print_gameboard();
 }
--- 3_Implementation/src/calculator_operations.h
+++ 3_Implementation/src/calculator_operations.h
@@ -8,6 +8,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
 //#include <stdio_ext.h>
 
 #define n 4
@@ -20,17 +23,20 @@ void setup_gameboard();
 void slam_left();
 //join move terms left 3 times till there is no coliisons
 
-void compress_left(int *gameboard);
+void compress_left();
 //if two of them are the same join and leave the last right one empty
 
 void print_gameboard();
 //print in pretty format
 
-void transpose(int *gameboard);
+void transpose();
 //transpose of 2d array
 
-void reverse(int *gameboard);
+void reverse();
 //reverse the 2d array
+
+void pick_random_tile();
+//place random 
 
 
 
